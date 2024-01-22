887043ecdd7ccbb2ca165f2a70a2304c364b9d50
Miguel Ringoot <miguel.ringoot@student.odisee.be>
2024-01-22 22:49:55 +0100

    css fix activiteit

--- fietsbook/activity/register.php
+++ fietsbook/activity/register.php
@@ -41,6 +41,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+    <link rel="stylesheet" href="https://unkpg.com/@csstools/normalize.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/activity.css">
 </head>
 <body>
 <input type="checkbox" id="menu-toggle">
